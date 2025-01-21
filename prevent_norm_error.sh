d46ce00195b946ec91ec168257d44c8543e12453
ceskelito <ceschelr@gmail.com>
2025-01-21 15:28:52 +0100

    Add files via upload

---
+++ prevent_norm_error.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+files="*.c *.h */*.c */*.h"
+file_count=$(find $files -type f | wc -l)
+norme_count=$(norminette | wc -l)
+
+if [ "$file_count" -ne "$norme_count" ]; then
+    diff=($norme_count - $file_count)
+    echo -e "\n\033[31;1;4mPossibili $diff errori di norma!\033[0m\n"
+    git push $@
+    exit 1
+fi
+exit 0
+
+	
